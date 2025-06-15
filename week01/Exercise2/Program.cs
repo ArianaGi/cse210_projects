a6f6687c4fe7811a3145539a3d3fc8fadca5294c
ArianaGi <gir23002@byui.edu>
2025-06-14 21:03:36 -0600

    W01Exercise2 Completed

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
-    }
-}
+        Console.WriteLine("Please enter your grade percentage: ");
+        string grade = Console.ReadLine();
+        int gradepercentage = int.Parse(grade);
+        char letter;
+
+        if (gradepercentage >= 90)
+        {
+            letter = 'A';
+        }
+        else if (gradepercentage >= 80)
+        {
+            letter = 'B';
+        }
+        else if (gradepercentage >= 70)
+        {
+            letter = 'C';
+        }
+        else if (gradepercentage >= 60)
+        {
+            letter = 'D';
+        }
+        else
+        {
+            letter = 'F';
+        }
+
+        Console.WriteLine($"You received a grade of {letter}.");
+
+        if (gradepercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class.");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately, you did not pass the class. Try again next semester.");
+        }
+    }    }
