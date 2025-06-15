46cb8f5379b6e96e7280c9ce6669f5de450b32d2
ArianaGi <gir23002@byui.edu>
2025-06-14 21:53:17 -0600

    w01Exercise 3 Completed

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Generate a random number between 1 and 100
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        int guessednumber;
+
+        Console.WriteLine("I have selected a magic number between 1 and 100.");
+        Console.WriteLine("What is your guess?: ");
+        guessednumber = int.Parse(Console.ReadLine());
+
+        while (guessednumber != number)
+        {
+            if (guessednumber < number)
+            {
+                Console.WriteLine("Go higher!");
+            }
+            else
+            {
+                Console.WriteLine("Go lower!");
+            }
+
+            Console.WriteLine("What is your guess?: ");
+            guessednumber = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Congratulations! You guessed the magic number.");
     }
 }
