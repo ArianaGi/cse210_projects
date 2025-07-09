6f6407606c324260b601ff6e1a92fc8abdc9552b
ArianaGi <gir23002@byui.edu>
2025-07-08 22:09:21 -0600

    simple assignment created

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("Alisson Cornell", "Polynomials");
+
+        Console.WriteLine(assignment1.GetSummary());
     }
 }
---
+++ week05/Homework/assignment.cs
@@ -0,0 +1,17 @@
+class Assignment
+{
+    private string _name;
+    private string _topic;
+
+    public Assignment(string name, string topic)
+    {
+        _name = name;
+        _topic = topic;
+
+    }
+
+    public string GetSummary()
+    {
+        return $"Student's name: {_name}\n Topic: {_topic}";
+    }
+}
